ce04eda6b85a8df16e37a0496c13bbecc3cc9154
Harish0411 <hariharanramasamy98@gmail.com>
2023-02-28 20:51:39 +0530

    print names in n times

---
+++ main.js
@@ -0,0 +1,11 @@
+// Print a name N times
+
+var name=prompt("Enter your name");
+var number=Number(prompt("Number of times"));
+
+function no(){
+     for(i=1; i<=number; i++){
+	      document.write(name + "<br>");
+		  }
+	 }
+	 no();
